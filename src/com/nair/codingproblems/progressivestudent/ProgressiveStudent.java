bc3a2b7301cedf0f441ff7e5b3d28967c9577796
Pranav Nair <pranav@NairFamilyMacBookPro-1.local>
2018-01-19 15:43:44 -0600

    progressive student

---
+++ src/com/nair/codingproblems/progressivestudent/ProgressiveStudent.java
@@ -0,0 +1,8 @@
+package com.nair.codingproblems.progressivestudent;
+
+public class ProgressiveStudent {
+    public boolean scoresIncreasing(double[] scores) {
+
+        return true;
+    }
+}
---
+++ src/com/nair/codingproblems/progressivestudent/ProgressiveStudentDriver.java
@@ -0,0 +1,10 @@
+package com.nair.codingproblems.progressivestudent;
+
+public class ProgressiveStudentDriver {
+    public static void main(String[] args){
+        ProgressiveStudent s1 = new ProgressiveStudent();
+        double[] gpas = {2.0,3.5,4.0};
+        boolean status = s1.scoresIncreasing(gpas);
+        System.out.println("Status: "+status);
+    }
+}
