0c60bfa5fcb71e031163825bd1d7e6c56789ceb2
Pranav Nair <pranav@NairFamilyMacBookPro-1.local>
2018-01-20 12:39:19 -0600

    Fixed #1

--- src/com/nair/codingproblems/progressivestudent/ProgressiveStudent.java
+++ src/com/nair/codingproblems/progressivestudent/ProgressiveStudent.java
@@ -2,20 +2,9 @@ package com.nair.codingproblems.progressivestudent;
 
 public class ProgressiveStudent {
     public boolean scoresIncreasing(double[] scores) {
-        double initial = 0, comparison = 0;
-        boolean resultant=true;
-        for(int x = 0;x<scores.length;x++){
-            if (initial == 0){
-                initial = scores[x];
-            }
-            else{
-                initial = scores[x-1];
-            }
-            comparison = scores[x];
-            if (initial<=comparison){
-                resultant = true;
-            }
-            else {
+        boolean resultant = true;
+        for (int x = 1; x < scores.length; x++) {
+            if (scores[x - 1] > scores[x]) {
                 resultant = false;
                 break;
             }
