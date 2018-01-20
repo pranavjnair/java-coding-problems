6f1667e54eafb5fdbacccebfa9df20189cdb49bb
Pranav Nair <pranav@NairFamilyMacBookPro-1.local>
2018-01-20 12:28:04 -0600

    first problem done

--- src/com/nair/codingproblems/progressivestudent/ProgressiveStudent.java
+++ src/com/nair/codingproblems/progressivestudent/ProgressiveStudent.java
@@ -2,7 +2,24 @@ package com.nair.codingproblems.progressivestudent;
 
 public class ProgressiveStudent {
     public boolean scoresIncreasing(double[] scores) {
-
-        return true;
+        double initial = 0, comparison = 0;
+        boolean resultant=true;
+        for(int x = 0;x<scores.length;x++){
+            if (initial == 0){
+                initial = scores[x];
+            }
+            else{
+                initial = scores[x-1];
+            }
+            comparison = scores[x];
+            if (initial<=comparison){
+                resultant = true;
+            }
+            else {
+                resultant = false;
+                break;
+            }
+        }
+        return resultant;
     }
 }
--- src/com/nair/codingproblems/progressivestudent/ProgressiveStudentDriver.java
+++ src/com/nair/codingproblems/progressivestudent/ProgressiveStudentDriver.java
@@ -3,8 +3,8 @@ package com.nair.codingproblems.progressivestudent;
 public class ProgressiveStudentDriver {
     public static void main(String[] args){
         ProgressiveStudent s1 = new ProgressiveStudent();
-        double[] gpas = {2.0,3.5,4.0};
+        double[] gpas = {4.0,3.5,4.0};
         boolean status = s1.scoresIncreasing(gpas);
-        System.out.println("Status: "+ status + " changed. My name is Pranav.");
+        System.out.println("Status: "+ status);
     }
 }
